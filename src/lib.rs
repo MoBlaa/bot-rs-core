20aab5f5ed05e0a561f1bbc8ccc4de17e446c80b
mo_blaa <moblaa@gmx.net>
2020-05-06 21:26:17 +0200

    implemented basic command system

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,40 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+use irc_rust::message::Message;
+
+pub static CORE_VERSION: &str = env!("CARGO_PKG_VERSION");
+pub static RUSTC_VERSION: &str = env!("RUSTC_VERSION");
+
+pub trait Command {
+    fn call(&self, args: &[&str]) -> Result<&str, InvocationError>;
+
+    fn help(&self) -> Option<&str> {
+        None
+    }
+}
+
+pub enum InvocationError {
+    InvalidArgumentCount { expected: usize, found: usize },
+    Other { msg: String }
+}
+
+pub struct CommandDeclaration {
+    pub rustc_version: &'static str,
+    pub core_version: &'static str,
+    pub register: unsafe extern "C" fn(&mut dyn CommandRegistrar),
+}
+
+pub trait CommandRegistrar {
+    fn register_command(&mut self, name: &str, function: Box<dyn Function>);
+}
+
+#[macro_export]
+macro_rules! export_command {
+    ($register:expr) => {
+        #[doc(hidden)]
+        #[no_mangle]
+        pub static command_declaration: $crate::CommandDeclaration = $crate::CommandDeclaration {
+            rustc_version: $crate::RUSTC_VERSION,
+            core_version: $crate::CORE_VERSION,
+            register: $register
+        }
     }
 }
